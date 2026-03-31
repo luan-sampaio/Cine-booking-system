a2c06378ac00cbb3d412b60e903b4403c0339db9
Luan Sampaio <luansampaio0294@gmail.com>
2026-03-31 08:21:31 -0300

    chore: inicializa projeto spring boot com dependencias base e docker-compose

---
+++ src/test/java/com/portfolio/cinebooking/CineBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.cinebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
